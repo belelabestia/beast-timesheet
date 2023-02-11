5c074a3e1fb9a248d168d9d74dff21860999f047
Marco Bellezza <jasone.mb@outlook.com>
2023-02-11 16:18:59 +0100

    add deploy assets

---
+++ push.ps1
@@ -0,0 +1,9 @@
+rm -Recurse -Force ./bts-belelabestia-it
+mkdir ./bts-belelabestia-it
+cp ./docker-compose.yml ./bts-belelabestia-it
+cp ./Caddyfile ./bts-belelabestia-it
+docker save beast-timesheet-app -o ./bts-belelabestia-it/beast-timesheet.image.tar
+scp -rC ./bts-belelabestia-it root@belelabestia.it:./apps
+ssh root@belelabestia.it docker load -i ./apps/bts-belelabestia-it/beast-timesheet.image.tar
+ssh root@belelabestia.it docker compose -f ./apps/bts-belelabestia-it/docker-compose.yml up -d
+ssh root@belelabestia.it caddy reload
