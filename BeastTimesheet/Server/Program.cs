137f0131a1c7ddd2eafa11b756c1ddb39a6cf9ae
Marco Bellezza <jasone.mb@outlook.com>
2023-02-11 19:00:09 +0100

    clean Program.cs

--- BeastTimesheet/Server/Program.cs
+++ BeastTimesheet/Server/Program.cs
@@ -1,8 +1,6 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddNpgsql<BloggingContext>(Config.DB_CONNECTION_STRING);
 builder.Services.AddControllersWithViews();
@@ -10,7 +8,6 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -18,20 +15,24 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<BloggingContext>();
+    db.Database.Migrate();
+
+    db.Blogs.ExecuteDelete();
+
+    db.Blogs.Add(new Blog { BlogId = 1, Url = "https://test.url" });
+    db.SaveChanges();
+}
 
 app.Run();
