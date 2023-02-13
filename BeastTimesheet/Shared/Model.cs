51685c42e34dff15947c49bf3b4d9eb65f32cece
Marco Bellezza <jasone.mb@outlook.com>
2023-02-13 20:33:58 +0100

    add app model

--- BeastTimesheet/Shared/Model.cs
+++ BeastTimesheet/Shared/Model.cs
@@ -1,3 +1,44 @@
+namespace BeastTimesheet.Shared.Models;
+
+public class Project
+{
+    public string Name { get; set; } = "";
+    public Decimal HourlyFee { get; set; }
+    IEnumerable<Timesheet>? Timesheets { get; set; }
+}
+
+public enum TimesheetState { Open, Closed }
+
+public class Timesheet
+{
+    public string Name { get; set; } = "";
+    public TimesheetState State { get; set; }
+    public Project? Project { get; set; }
+}
+
+public class Session
+{
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly StopTime { get; set; }
+    public TimeSpan BreaksTime { get; set; }
+    public TimeSpan GrossTime => StopTime - StartTime;
+    public TimeSpan NetTime => GrossTime - BreaksTime;
+    public Decimal SessionFee => Convert.ToDecimal(NetTime.TotalHours) * Timesheet!.Project!.HourlyFee;
+    public Timesheet? Timesheet { get; set; }
+}
+
+public enum BillState { Emitted, Payed, Expired }
+
+public class Bill
+{
+    public DateTime ExpirationDate { get; set; }
+    public bool Expired => DateTime.Now > ExpirationDate;
+    public bool Payed { get; set; }
+}
+
+/**************************************/
+
 public class Blog
 {
     public int BlogId { get; set; }
