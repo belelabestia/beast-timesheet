8a2e63317ec1da620fabe2299b84b540dbd48fe9
Marco Bellezza <jasone.mb@outlook.com>
2023-02-13 23:51:42 +0100

    add project crud

--- BeastTimesheet/Server/Controllers/ProjectController.cs
+++ BeastTimesheet/Server/Controllers/ProjectController.cs
@@ -18,8 +18,51 @@ public class ProjectsController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Project> Get()
+    public IEnumerable<Project> GetProjects()
     {
         return _context.Projects!;
+    }
+
+    [HttpGet("{id:int}")]
+    public Project? GetProject(int id)
+    {
+        return _context.Projects!.SingleOrDefault(p => p.Id == id);
+    }
+    
+    [HttpPost]
+    public Project CreateProject(Project project)
+    {
+        _context.Projects!.Add(project);
+        _context.SaveChanges();
+
+        return project;
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateProject(int id, Project project)
+    {
+        if (!_context.Projects!.Any(p => p.Id == id))
+        {
+            return NotFound();
+        }
+
+        _context.Projects!.Update(project);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteProject(int id)
+    {
+        if (!_context.Projects!.Any(p => p.Id == id))
+        {
+            return NotFound();
+        }
+
+        _context.Projects!.Remove(new Project { Id = id });
+        await _context.SaveChangesAsync();
+
+        return Ok();
     }
 }
