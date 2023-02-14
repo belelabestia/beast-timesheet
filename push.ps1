07a3de5a4194dffae66a8f0a8c19a1f3d7850aaa
Marco Bellezza <jasone.mb@outlook.com>
2023-02-14 19:35:33 +0100

    add system prune after push

--- push.ps1
+++ push.ps1
@@ -6,4 +6,5 @@ docker save beast-timesheet-app -o ./bts-belelabestia-it/beast-timesheet.image.t
 scp -rC ./bts-belelabestia-it root@belelabestia.it:./apps
 ssh root@belelabestia.it docker load -i ./apps/bts-belelabestia-it/beast-timesheet.image.tar
 ssh root@belelabestia.it docker compose -f ./apps/bts-belelabestia-it/docker-compose.yml up -d
+ssh root@belelabestia.it docker system prune
 ssh root@belelabestia.it caddy reload
