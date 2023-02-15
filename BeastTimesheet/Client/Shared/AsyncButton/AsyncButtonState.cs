0a8ac4ffb75642d3797cbc9f8d6ab654b0bfae57
Marco Bellezza <jasone.mb@outlook.com>
2023-02-15 23:47:05 +0100

    improve button styles

--- BeastTimesheet/Client/Shared/AsyncButton/AsyncButtonState.cs
+++ BeastTimesheet/Client/Shared/AsyncButton/AsyncButtonState.cs
@@ -1,1 +1,11 @@
 public enum AsyncButtonState { Ready, Loading, Error }
+
+public static class AsyncButtonStateExtensions
+{
+    public static AsyncButtonState Merge(this AsyncButtonState first, AsyncButtonState second) =>
+        first is AsyncButtonState.Loading || second is AsyncButtonState.Loading
+            ? AsyncButtonState.Loading
+            : first is AsyncButtonState.Error || second is AsyncButtonState.Error
+            ? AsyncButtonState.Error
+            : AsyncButtonState.Ready;
+}
