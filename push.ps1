0d62aa647102be873779490a7c1c9f3a90302ce3
Marco Bellezza <jasone.mb@outlook.com>
2023-02-17 01:04:42 +0100

    update push script

--- push.ps1
+++ push.ps1
@@ -5,6 +5,6 @@ cp ./Caddyfile ./bts-belelabestia-it
 docker save beast-timesheet-app -o ./bts-belelabestia-it/beast-timesheet.image.tar
 scp -rC ./bts-belelabestia-it root@belelabestia.it:./apps
 ssh root@belelabestia.it docker load -i ./apps/bts-belelabestia-it/beast-timesheet.image.tar
-ssh root@belelabestia.it docker compose -f ./apps/bts-belelabestia-it/docker-compose.yml up -d
+ssh root@belelabestia.it docker compose -f ./apps/bts-belelabestia-it/docker-compose.yml up -d --force-recreate
 ssh root@belelabestia.it docker system prune
 ssh root@belelabestia.it caddy reload
