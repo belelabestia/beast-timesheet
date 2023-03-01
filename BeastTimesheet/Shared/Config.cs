3c876bb0ae824e5999020cf1af7bf13b32c7075b
Marco Bellezza <jasone.mb@outlook.com>
2023-03-01 14:51:29 +0100

    v0.2.3

--- BeastTimesheet/Shared/Config.cs
+++ BeastTimesheet/Shared/Config.cs
@@ -11,5 +11,5 @@ public static class Config
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
     };
 
-    public const string VERSION = "0.2.2";
+    public const string VERSION = "0.2.3";
 }
